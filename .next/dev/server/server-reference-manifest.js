d0c0e1e85ef4a8ca74b5a4a72dc8c9853a7549ab
Satyajit Shetye <sshetye@Satyajits-MacBook-Pro.local>
2026-06-06 19:28:47 +0530

    Ignore next

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XhmDdfpCn0ItZHdD0gVV6l21gOXwhYnLsom+LzGyrLE=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d8ef5f8d._.css"
-],
-    source: "dynamic"
-});
